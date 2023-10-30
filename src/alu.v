aceec526d8ada33beae3b83e6b270c574e51c898
r-smalec <rsmalec@gmail.com>
2023-10-30 13:26:37 +0100

    alu.v chaned nameing convention

--- src/alu.v
+++ src/alu.v
@@ -1,10 +1,12 @@
+`include "instructions.v"
+
 module alu(
     input               clk,
     input       [3:0]   alu_instruction_code,
     input       [7:0]   acu,
     input       [7:0]   reg_file,
 
-    output reg  [7:0]   data,
+    output reg  [7:0]   acumulator,
     output reg          flag_z,
     output reg          flag_cy,
     output reg          flag_ov,
@@ -14,21 +16,21 @@ module alu(
 
 always @ (posedge clk) begin
     case(alu_instruction_code)
-        `NOT:   data <= ~acu;
-        `XOR:   data <= acu ^ reg_file;
-        `OR:    data <= acu | reg_file;
-        `AND:   data <= acu & reg_file;
-        `SUB:   data <= acu - reg_file;
-        `ADD:   {flag_cy, data} <= acu + reg_file;
-        `RR:    data <= acu >> 1;
-        `RL:    data <= acu << 1;
-        `DEC:   data <= acu - 1;
-        `INC:   data <= acu + 1;
+        `NOT:   acumulator <= ~acu;
+        `XOR:   acumulator <= acu ^ reg_file;
+        `OR:    acumulator <= acu | reg_file;
+        `AND:   acumulator <= acu & reg_file;
+        `SUB:   acumulator <= acu - reg_file;
+        `ADD:   {flag_cy, acumulator} <= acu + reg_file;
+        `RR:    acumulator <= acu >> 1;
+        `RL:    acumulator <= acu << 1;
+        `DEC:   acumulator <= acu - 1;
+        `INC:   acumulator <= acu + 1;
     endcase
 
-    flag_z <= data == 0 ? 1'b1 : 1'b0;
-    flag_p <= ~^data;
-    falg_s <= data[7];
+    flag_z <= acumulator == 0 ? 1'b1 : 1'b0;
+    flag_p <= ~^acumulator;
+    flag_s <= acumulator[7];
 end
 
 endmodule
